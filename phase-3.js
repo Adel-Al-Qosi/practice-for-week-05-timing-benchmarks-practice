3434326484074ad871edc98b4a8d1e023601a519
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-03-16 19:42:25 +0300

    finish practice

--- phase-3.js
+++ phase-3.js
@@ -5,25 +5,38 @@ function addNums10Timing(increment) {
   // Then, add timing code
 
   // Your code here
+  let start = Date.now();
+  let result = [];
 
+  for (let i = increment; i <= increment * 10; i += increment) {
+    result.push(addNums(i));
+  }
+  let end = Date.now();
+
+  return end - start
 }
-
 
 function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
-// Then, add timing code
+  // Copy your `addManyNums10` code here
+  // Then, add timing code
 
   // Your code here
+  let start = Date.now();
+  let result = [];
 
+  for (let i = increment; i <= increment * 10; i += increment) {
+    result.push(addManyNums(i));
+  }
+  let end = Date.now();
+
+  return end - start
 }
 
-
-n = 1000000
-console.log(`addNums(${n}): `);
-addNums10Timing(1000000);
+n = 1000000;
+console.log(`addNums(${n}): ${addNums10Timing(1000000)} ms`);
 
 console.log("\n***********\n");
 
-n = 1000
-console.log(`addManyNums(${n}): `);
-addManyNums10Timing(5000);
+n = 1000;
+console.log(`addManyNums(${n}): ${addManyNums10Timing(5000)} ms`);
+
